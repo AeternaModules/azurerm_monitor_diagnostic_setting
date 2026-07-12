87597eeecaaf359a91bc9bd6393753062fd2a253
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,45 @@
-output "monitor_diagnostic_settings" {
-  description = "All monitor_diagnostic_setting resources"
-  value       = azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings
-}
 output "monitor_diagnostic_settings_enabled_log" {
-  description = "List of enabled_log values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.enabled_log]
+  description = "Map of enabled_log values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.enabled_log }
 }
 output "monitor_diagnostic_settings_enabled_metric" {
-  description = "List of enabled_metric values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.enabled_metric]
+  description = "Map of enabled_metric values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.enabled_metric }
 }
 output "monitor_diagnostic_settings_eventhub_authorization_rule_id" {
-  description = "List of eventhub_authorization_rule_id values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.eventhub_authorization_rule_id]
+  description = "Map of eventhub_authorization_rule_id values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.eventhub_authorization_rule_id }
 }
 output "monitor_diagnostic_settings_eventhub_name" {
-  description = "List of eventhub_name values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.eventhub_name]
+  description = "Map of eventhub_name values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.eventhub_name }
 }
 output "monitor_diagnostic_settings_log_analytics_destination_type" {
-  description = "List of log_analytics_destination_type values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.log_analytics_destination_type]
+  description = "Map of log_analytics_destination_type values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.log_analytics_destination_type }
 }
 output "monitor_diagnostic_settings_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.log_analytics_workspace_id }
 }
 output "monitor_diagnostic_settings_metric" {
-  description = "List of metric values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.metric]
+  description = "Map of metric values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.metric }
 }
 output "monitor_diagnostic_settings_name" {
-  description = "List of name values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.name]
+  description = "Map of name values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.name }
 }
 output "monitor_diagnostic_settings_partner_solution_id" {
-  description = "List of partner_solution_id values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.partner_solution_id]
+  description = "Map of partner_solution_id values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.partner_solution_id }
 }
 output "monitor_diagnostic_settings_storage_account_id" {
-  description = "List of storage_account_id values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.storage_account_id]
+  description = "Map of storage_account_id values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.storage_account_id }
 }
 output "monitor_diagnostic_settings_target_resource_id" {
-  description = "List of target_resource_id values across all monitor_diagnostic_settings"
-  value       = [for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : v.target_resource_id]
+  description = "Map of target_resource_id values across all monitor_diagnostic_settings, keyed the same as var.monitor_diagnostic_settings"
+  value       = { for k, v in azurerm_monitor_diagnostic_setting.monitor_diagnostic_settings : k => v.target_resource_id }
 }
 
