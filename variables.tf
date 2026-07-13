c88620f070a253bd95d82a434d344bf7acf39aab
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:35:40 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -49,7 +49,7 @@ EOT
     })))
     metric = optional(list(object({
       category = string
-      enabled  = optional(bool) # Default: true
+      enabled  = optional(bool)
       retention_policy = optional(object({
         days    = optional(number)
         enabled = bool
